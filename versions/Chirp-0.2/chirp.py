64581d3d6c7ede3ef6140aef514a989c22d8cdb9
node5 <neom.bash@gmail.com>
2017-08-07 12:33:19 -0700

    Added other commands

--- versions/Chirp-0.2/chirp.py
+++ versions/Chirp-0.2/chirp.py
@@ -1,39 +1,54 @@
-'Chirp, host a simple chat server. Written by Kale Champagnie <node5@github.com>'
-from app import init
-from app import start
+'Chirp, host your own chat server. Written by Kale Champagnie <node5@github.com>'
 from sys import argv
-from sys import stdout
-from sys import stdin
-from sys import stderr
+from app import create
+from app import start
+from app import change
+from app import servers
+from app import delete
 
 
 
 def main(args): 
     if (len(args) < 2):
-        stderr.write('''
+        print('''
 usage:
    chirp <command>
 
 purpose:
-   Chirp lets you host a simple chat server on your network. Once a user connects, they can choose to create or join a chat room.
-   Users connect to your server via chirp clients such as, Eime. Clients are available to download from github.com/node5/Chirp/clients.
+   Chirp lets you host chat servers on your network. Once a user connects, they can choose to create or join a room.
+   Users connect to your server via chirp clients such as Eime. Clients are available to download from github.com/node5/Chirp/clients.
 
 commands:
-   init    create and configure your server
-   start   start your server and allow people to join
+   create   create a new server
+   start    start a server, allow clients to connect      
+   change   change a server's settings
+   list     list existing servers
+   delete   delete a server
+   
+author:
+   Kale Champagnie <node5@github.com>
 ''')
         
     elif (len(args) > 2):
-        stderr.write('chirp: to many arguments were given')
+        print('chirp: to many arguments were given')
           
     else:
         command = args[1]
 
-        if (command == 'init'):
-            init.main()
+        if (command == 'create'):
+            create.main()
 
         elif (command == 'start'):
             start.main()
+
+        elif (command == 'change'):
+            change.main()
+
+        elif (command == 'servers'):
+            servers.main()
+
+        elif (command == 'delete'):
+            delete.main()
           
         else:
             stderr.write('chirp: unknown command')
